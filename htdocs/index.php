17dcf1d3ee2db316d8946e3910d17b442d147f41
EC2 Default User <ec2-user@ip-172-31-20-4.ec2.internal>
2017-12-01 20:08:06 +0000

    Updated the index.php file

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,8 @@
 <?php
-echo "<html>";
-echo "<head></head>";
-echo "<body>";
-echo "<p>Hello World! Julian and Caleb rule!</p>";
-echo "</body>";
-echo "</html>";
-?>
+echo "<html>\n";
+echo "<head></head>\n";
+echo "<body>\n";
+echo "<p>Hello World! Julian and Caleb rule!</p>\n";
+echo "</body>\n";
+echo "</html>\n";
+
